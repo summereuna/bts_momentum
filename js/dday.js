06777dbd06d47602a888b57726cb0df1db845091
summereuna <summereuna@gmail.com>
2024-05-09 18:39:12 +0900

    Feat dday

--- js/dday.js
+++ js/dday.js
@@ -1,27 +1,61 @@
 const dDay = document.querySelector("#dDay");
+
 const dDayForm = document.querySelector("#dDay-form");
-const dDaySet = dDayForm.querySelector("input");
+const dDayInput = dDayForm.querySelector("input");
 const dDaySaveBtn = dDayForm.querySelector("button");
-const dDayCount = dDay.querySelector("span");
 
-const HIDDEN_CLASSNAME2 = "hidden";
+const dDayTextBox = document.querySelector("#dDay-date");
+const dDayCountText = dDayTextBox.querySelector("span");
+const dDayRemoveBtn = dDayTextBox.querySelector("button");
 
-function saveDDay(event){
-    event.preventDefault();
-    dDayForm.classList.add(HIDDEN_CLASSNAME2);
-    dDayCount.classList.remove(HIDDEN_CLASSNAME2);
-    
-    const comeback = new Date(dDaySet.valueAsDate);
-    const now = new Date();
-    
-    const untilDDay = comeback.getTime() - now.getTime();
-    const days = Math.floor(untilDDay / (1000*60*60*24));
-    
-    dDayCount.innerText = `${days}일 남았어요!`;
+const HIDDEN_CLASSNAME_2 = "hidden";
+const DDAY_KEY = "comebackDDay";
 
-    getDDay();
-    setInterval(getDDay, 1000);
+function onSaveDDay(event) {
+  event.preventDefault();
+  dDayForm.classList.add(HIDDEN_CLASSNAME_2);
+
+  const comeback = new Date(dDayInput.valueAsDate);
+  const now = new Date();
+  const newDDay = comeback.getDate() - now.getDate();
+
+  localStorage.setItem(DDAY_KEY, newDDay);
+
+  paintDDay(newDDay);
 }
 
+function removeDDay(event) {
+  console.log(event);
+  localStorage.removeItem(DDAY_KEY);
+  dDayTextBox.classList.add(HIDDEN_CLASSNAME_2);
+  dDayForm.classList.remove(HIDDEN_CLASSNAME_2);
+}
 
-dDayForm.addEventListener("submit", saveDDay);
+function paintDDay(newDDay) {
+  if (newDDay < 0) {
+    localStorage.removeItem(DDAY_KEY);
+    alert(
+      "이전 날짜를 디데이로 설정할 수 없습니다. 디데이를 새로 설정해 주세요."
+    );
+    dDayForm.classList.remove(HIDDEN_CLASSNAME_2);
+
+    return;
+  }
+
+  dDayCountText.innerText = `${
+    Number(newDDay) === 0 ? "오늘이에요!" : `${newDDay}일 남았어요!`
+  }`;
+
+  dDayTextBox.classList.remove(HIDDEN_CLASSNAME_2);
+
+  dDayRemoveBtn.addEventListener("click", removeDDay);
+}
+
+const savedDDay = localStorage.getItem(DDAY_KEY);
+
+if (savedDDay === null) {
+  dDayForm.classList.remove(HIDDEN_CLASSNAME_2);
+  dDayForm.addEventListener("submit", onSaveDDay);
+} else {
+  paintDDay(savedDDay);
+}
